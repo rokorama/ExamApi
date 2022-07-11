43057d614cc135c4bdf946a1ea8fe89fd1bd6449
Rokas <rokamanauskas@gmail.com>
2022-07-11 20:36:45 +0300

    Add DB context class, business logic class lib

---
+++ ExamApi.DataAccess/Contexts/AppDbContext.cs
@@ -0,0 +1,16 @@
+using ExamApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExamApi.DataAccess;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<PersonalInfo> PersonalInfos { get; set; }
+    public DbSet<PlaceOfResidence> PlacesOfResidence { get; set; }
+}
