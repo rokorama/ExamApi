e1588732ad1fe80dfc6797cac89a77976a50eb31
Rokas <rokamanauskas@gmail.com>
2022-07-12 18:40:50 +0300

    Add basic signup functionality

--- ExamApi.UserAccess/Services/UserService.cs
+++ ExamApi.UserAccess/Services/UserService.cs
@@ -1,6 +1,31 @@
+using ExamApi.DataAccess;
+using ExamApi.Models;
+
 namespace ExamApi.UserAccess;
 
 public class UserService : IUserService
 {
-    
+    private IUserRepository _userRepo;
+
+    public UserService(IUserRepository userRepo)
+    {
+        _userRepo = userRepo;
+    }
+
+    public User CreateUser(string username, string password)
+    {
+        CreatePassword(password, out string passwordHash);
+        var user = new User()
+        {
+            Username = username,
+            Password = passwordHash,
+            Role = "User"
+        };
+        return _userRepo.AddNewUser(user);
+    }
+
+    private void CreatePassword(string password, out string passwordHash)
+    {
+        passwordHash = BCrypt.Net.BCrypt.HashPassword(password);
+    }
 }
