587f601967361a34eedaaab71db773bb31fd4701
Rokas <rokamanauskas@gmail.com>
2022-07-12 20:41:11 +0300

    Add DTOs for personal info-related objects

---
+++ ExamApi.Models/DTOs/PersonalInfoDto.cs
@@ -0,0 +1,9 @@
+namespace ExamApi.Models;
+
+public class PersonalInfoDto
+{
+    public string FirstName  { get; set; }
+    public string LastName  { get; set; }
+    public int PersonalNumber { get; set; }
+    public string Email { get; set; }
+}
---
+++ ExamApi.Models/DTOs/PlaceOfResidenceDto.cs
@@ -0,0 +1,9 @@
+namespace ExamApi.Models;
+
+public class PlaceOfResidenceDto
+{
+    public string City { get; set; }
+    public string Street { get; set; }
+    public string House { get; set; }
+    public int? Flat { get; set; }
+}
