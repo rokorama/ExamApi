4db63f69a2e7441abea2ebb68794cddcfdcbc89f
Rokas <rokamanauskas@gmail.com>
2022-07-12 18:26:36 +0300

    Add user repository with basic signup/login functionality

--- ExamApi.DataAccess/Services/UserRepository.cs
+++ ExamApi.DataAccess/Services/UserRepository.cs
@@ -1,6 +1,38 @@
+using ExamApi.Models;
+
 namespace ExamApi.DataAccess;
 
 public class UserRepository : IUserRepository
 {
-    
+    private AppDbContext _dbContext;
+
+    public UserRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public User GetUser(string username)
+    {
+        return _dbContext.Users.FirstOrDefault(x => x.Username == username);
+    }
+
+    // no nulls please
+    public User AddNewUser(User user)
+    {
+        _dbContext.Users.Add(user);
+        if (_dbContext.SaveChanges() > 0)
+            return user;
+        else
+            return null;
+    }
+
+    public bool GrantAdminRights(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool RevokeUserRights(User user)
+    {
+        throw new NotImplementedException();
+    }
 }
