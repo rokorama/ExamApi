c0ea01d1eb8d901a22bed1d76bbbe000c55dfa54
Rokas <rokamanauskas@gmail.com>
2022-07-13 19:33:53 +0300

    Add CRUD methods to residence info repo

--- ExamApi.DataAccess/Services/IResidenceInfoRepository.cs
+++ ExamApi.DataAccess/Services/IResidenceInfoRepository.cs
@@ -1,6 +1,11 @@
+using ExamApi.Models;
+
 namespace ExamApi.DataAccess;
 
 public interface IResidenceInfoRepository
 {
-
+    public ResidenceInfo GetInfo(Guid id);
+    public void AddInfo(ResidenceInfo info);
+    public void EditInfo(ResidenceInfo info);
+    public void DeleteInfo(Guid id);
 }
--- ExamApi.DataAccess/Services/ResidenceInfoRepository.cs
+++ ExamApi.DataAccess/Services/ResidenceInfoRepository.cs
@@ -1,6 +1,59 @@
+using ExamApi.Models;
+
 namespace ExamApi.DataAccess;
 
 public class ResidenceInfoRepository : IResidenceInfoRepository
 {
-    
+    private readonly AppDbContext _dbContext;
+
+    public ResidenceInfoRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public void AddInfo(ResidenceInfo info)
+    {
+        _dbContext.ResidenceInfos.Add(info);
+        try
+        {
+            _dbContext.SaveChanges();
+        }
+        catch (Exception)
+        {
+            // ????
+        }
+    }
+
+    public void DeleteInfo(Guid id)
+    {
+        var entryToDelete = _dbContext.ResidenceInfos.Find(id);
+        _dbContext.ResidenceInfos.Remove(entryToDelete);
+        try
+        {
+            _dbContext.SaveChanges();
+        }
+        catch (Exception)
+        {
+            // ????
+        }
+    }
+
+    public void EditInfo(ResidenceInfo updatedInfo)
+    {
+        var dbEntry = _dbContext.ResidenceInfos.SingleOrDefault(a => a.Id == updatedInfo.Id);
+        _dbContext.Entry(dbEntry).CurrentValues.SetValues(updatedInfo);
+        try
+        {
+            _dbContext.SaveChanges();
+        }
+        catch (Exception)
+        {
+            // ????
+        }
+    }
+
+    public ResidenceInfo GetInfo(Guid id)
+    {
+        return _dbContext.ResidenceInfos.Find(id);
+    }
 }
