e71b89573801c7d04073a56b8dfdbfeefe27e802
Rokas <rokamanauskas@gmail.com>
2022-07-25 22:01:11 +0300

    Add FluentValidation library for validating user input

---
+++ ExamApi.BusinessLogic/Helpers/PersonalInfoValidator.cs
@@ -0,0 +1,17 @@
+using ExamApi.Models.UploadRequests;
+using FluentValidation;
+
+namespace ExamApi.BusinessLogic.Helpers;
+
+public class PersonalInfoUploadRequestValidator : AbstractValidator<PersonalInfoUploadRequest>
+{
+    public PersonalInfoUploadRequestValidator()
+    {
+        RuleFor(personalInfo => personalInfo.FirstName).NotNull();
+        RuleFor(personalInfo => personalInfo.LastName).NotNull();
+        RuleFor(personalInfo => personalInfo.PersonalNumber.ToString()).NotNull().Length(11);
+        RuleFor(personalInfo => personalInfo.Email).NotNull();
+        RuleFor(personalInfo => personalInfo.ImageUpload).NotNull();
+        RuleFor(personalInfo => personalInfo.Address!).SetValidator(new AddressDtoValidator());
+    }
+}
