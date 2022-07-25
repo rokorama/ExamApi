aa7d6e0a0c2009bcabe253b7091986987fd60f7b
Rokas <rokamanauskas@gmail.com>
2022-07-25 19:18:59 +0300

    Add new test and fix old ones to reflect changes in helper architecture

--- ExamApi.Tests/BusinessLogic/Services/PersonalInfoServiceTests.cs
+++ ExamApi.Tests/BusinessLogic/Services/PersonalInfoServiceTests.cs
@@ -1,8 +1,12 @@
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using AutoFixture.Xunit;
 using ExamApi.BusinessLogic;
 using ExamApi.BusinessLogic.Helpers;
 using ExamApi.DataAccess;
 using ExamApi.Models;
 using ExamApi.Models.DTOs;
+using ExamApi.Models.UploadRequests;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Xunit;
@@ -12,27 +16,34 @@ namespace ExamApi.Tests.BusinessLogic;
 public class PersonalInfoServiceTests
 {
     private readonly PersonalInfoService _sut;
+    private readonly IFixture _fixture;
     private readonly Mock<IPersonalInfoRepository> _repoMock = new Mock<IPersonalInfoRepository>();
     private readonly Mock<ILogger<PersonalInfoService>> _loggerMock = new Mock<ILogger<PersonalInfoService>>();
+    private readonly Mock<IObjectMapper> _mapperMock = new Mock<IObjectMapper>();
+    private readonly Mock<IPropertyChanger> _propertyChangerMock = new Mock<IPropertyChanger>();
 
     
     public PersonalInfoServiceTests()
     {
-        _sut = new PersonalInfoService(_repoMock.Object, _loggerMock.Object);
+        _sut = new PersonalInfoService(_repoMock.Object,
+                                       _loggerMock.Object,
+                                       _mapperMock.Object,
+                                       _propertyChangerMock.Object);
+        _fixture = new Fixture().Customize(new AutoMoqCustomization { ConfigureMembers = true });
     }
 
     [Fact]
-    public void GetInfo_WorksCorrectly_WhenUserExists()
+    public void GetInfo_ReturnsDto_WhenUserExists()
     {
         // Arrange
-        var userId = Guid.NewGuid();
-        var personalInfoId = Guid.NewGuid();
-        var personalInfoFirstName = "Steve";
+        Guid userId = Guid.NewGuid();
+        Guid personalInfoId = Guid.NewGuid();
+        string expectedFirstName = "Steve";
         // ???
         var personalInfo = new PersonalInfo()
         {
             Id = personalInfoId,
-            FirstName = personalInfoFirstName,
+            FirstName = expectedFirstName,
             LastName = It.IsAny<String>(),  
             PersonalNumber = It.IsAny<ulong>(),
             Email = It.IsAny<String>(),
@@ -40,35 +51,75 @@ public class PersonalInfoServiceTests
             Address = new Address()
         };
         _repoMock.Setup(r => r.GetInfo(userId)).Returns(personalInfo);
+        _mapperMock.Setup(m => m.MapPersonalInfoDto(personalInfo)).Returns(new PersonalInfoDto() { FirstName = expectedFirstName} );
 
         // Act
-        var mockPersonalInfoDto = _sut.GetInfo(userId);
+        PersonalInfoDto? mockPersonalInfoDto = _sut.GetInfo(userId);
         
         // Assert
-        Assert.Equal(mockPersonalInfoDto!.FirstName, personalInfoFirstName);
+        Assert.Equal(mockPersonalInfoDto!.FirstName, expectedFirstName);
     }
 
-    // get info when info does not exist
-
     [Fact]
-    public void GetInfo_WorksCorrectly_WhenUserIsNonexistent()
+    public void GetInfo_ReturnsNull_WhenUserIsNonexistent()
     {
         // Arrange
         _repoMock.Setup(r => r.GetInfo(It.IsAny<Guid>())).Returns(value: null);
 
         // Act
-        var mockPersonalInfoDto = _sut.GetInfo(It.IsAny<Guid>());
+        PersonalInfoDto? mockPersonalInfoDto = _sut.GetInfo(It.IsAny<Guid>());
         
         // Assert
         Assert.Null(mockPersonalInfoDto);
     }
 
-    // AddInfo(PersonalInfoUploadRequest uploadRequest, Guid userId, out PersonalInfo? createdEntry);
-        // with valid info
     [Fact]
-    public void AddInfo
+    public void GetInfo_ReturnsNull_WhenUserExistsAndInfoIsNull()
+    {
+        // Arrange
+        _repoMock.Setup(r => r.GetInfo(It.IsAny<Guid>())).Returns(value: null);
+
+        // Act
+        PersonalInfoDto? mockPersonalInfoDto = _sut.GetInfo(It.IsAny<Guid>());
+
+        //Assert
+        Assert.Null(mockPersonalInfoDto);
+    }
+
+    [Fact, AutoData]
+    public void AddInfo_WorksCorrectly_WithValidInput()
+    {
+        // Arrange
+        Guid userId = _fixture.Create<Guid>();
+        var uploadRequest = _fixture.Create<PersonalInfoUploadRequest>();
+        uploadRequest.Email = "test@asdasd.asd";
+
+            // set up repo returns
+        _repoMock.Setup(r => r.AddInfo(It.IsAny<PersonalInfo>(), userId)).Returns(true);
+        _repoMock.Setup(r => r.CheckForExistingPersonalInfo(userId)).Returns(false);
+        
+            // set up image converter returns
+        var _imageConverterMock = new Mock<IImageConverter>();
+        var imageUploadRequestMock = _fixture.Create<ImageUploadRequest>();
+        _imageConverterMock.Setup(i => i.ConvertImage(imageUploadRequestMock)).Returns(It.IsAny<byte[]>());
+
+        var testPersonalInfo = _fixture.Create<PersonalInfo>();
+        testPersonalInfo.Email = "test@asdasd.asd";
+        _mapperMock.Setup(m => m.MapPersonalInfoUpload(It.IsAny<PersonalInfoUploadRequest>())).Returns(testPersonalInfo);
+        
+        // Act
+        _sut.AddInfo(uploadRequest, userId, out PersonalInfo? result);
+        
+        // Assert
+        Assert.Equal(uploadRequest.Email, result!.Email);
+    }
+
     // AddInfo(PersonalInfoUploadRequest uploadRequest, Guid userId, out PersonalInfo? createdEntry);
         // with invalid info
+
+
+    // AddInfo(PersonalInfoUploadRequest uploadRequest, Guid userId, out PersonalInfo? createdEntry);
+        // with existing info
 
 
 
