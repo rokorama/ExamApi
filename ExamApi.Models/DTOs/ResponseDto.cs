d97c694fa80ba22f85b867730e2f65e2fdd35cb5
Rokas <rokamanauskas@gmail.com>
2022-07-25 23:16:50 +0300

    Add 2 different constructors for ResponseDto

--- ExamApi.Models/DTOs/ResponseDto.cs
+++ ExamApi.Models/DTOs/ResponseDto.cs
@@ -4,4 +4,15 @@ public class ResponseDto
 {
     public bool Success { get; set; }
     public string? Message { get; set; }
+
+    public ResponseDto()
+    {
+        
+    }
+
+    public ResponseDto(bool success, string? message = null)
+    {
+        Success = success;
+        Message = message;
+    }
 }
