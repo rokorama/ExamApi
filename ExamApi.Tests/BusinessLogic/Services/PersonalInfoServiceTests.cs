02e2ecd3c8c04976a18a7856bda67fcf3154755e
Rokas <rokamanauskas@gmail.com>
2022-07-26 10:53:42 +0300

    Amend existing PersonalInfoService tests to reflect changes in validation

--- ExamApi.Tests/BusinessLogic/Services/PersonalInfoServiceTests.cs
+++ ExamApi.Tests/BusinessLogic/Services/PersonalInfoServiceTests.cs
@@ -7,6 +7,7 @@ using ExamApi.DataAccess;
 using ExamApi.Models;
 using ExamApi.Models.DTOs;
 using ExamApi.Models.UploadRequests;
+using FluentValidation;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Xunit;
@@ -21,6 +22,7 @@ public class PersonalInfoServiceTests
     private readonly Mock<ILogger<PersonalInfoService>> _loggerMock = new Mock<ILogger<PersonalInfoService>>();
     private readonly Mock<IObjectMapper> _mapperMock = new Mock<IObjectMapper>();
     private readonly Mock<IPropertyChanger> _propertyChangerMock = new Mock<IPropertyChanger>();
+    private readonly Mock<IValidator<PersonalInfoUploadRequest>> _infoUploadValidatorMock = new Mock<IValidator<PersonalInfoUploadRequest>>();
 
     
     public PersonalInfoServiceTests()
@@ -28,7 +30,8 @@ public class PersonalInfoServiceTests
         _sut = new PersonalInfoService(_repoMock.Object,
                                        _loggerMock.Object,
                                        _mapperMock.Object,
-                                       _propertyChangerMock.Object);
+                                       _propertyChangerMock.Object,
+                                       _infoUploadValidatorMock.Object);
         _fixture = new Fixture().Customize(new AutoMoqCustomization { ConfigureMembers = true });
     }
 
@@ -39,7 +42,6 @@ public class PersonalInfoServiceTests
         Guid userId = Guid.NewGuid();
         Guid personalInfoId = Guid.NewGuid();
         string expectedFirstName = "Steve";
-        // ???
         var personalInfo = new PersonalInfo()
         {
             Id = personalInfoId,
@@ -61,7 +63,7 @@ public class PersonalInfoServiceTests
     }
 
     [Fact]
-    public void GetInfo_ReturnsNull_WhenUserIsNonexistent()
+    public void GetInfo_ReturnsNull_WhenUserDoesNotExist()
     {
         // Arrange
         _repoMock.Setup(r => r.GetInfo(It.IsAny<Guid>())).Returns(value: null);
@@ -86,7 +88,37 @@ public class PersonalInfoServiceTests
         Assert.Null(mockPersonalInfoDto);
     }
 
-    [Fact, AutoData]
+
+    [Fact]
+    public void GetInfoId_ReturnsId_WhenDataExists()
+    {
+        // Arrange
+        var userId = _fixture.Create<Guid>();
+        var personalInfo = _fixture.Create<PersonalInfo>();
+        _repoMock.Setup(r => r.GetInfoId(userId)).Returns(personalInfo.Id);
+
+        // Act
+        var returnedId = _sut.GetInfoId(userId);
+
+        // Assert
+        Assert.Equal(personalInfo.Id, returnedId);
+    }
+
+    [Fact]
+    public void GetInfoId_ReturnsNull_WhenInfoDoesNotExist()
+    {
+        // Arrange
+        var userId = _fixture.Create<Guid>();
+        _repoMock.Setup(r => r.GetInfoId(userId)).Returns(value: null);
+
+        // Act
+        var returnedId = _sut.GetInfoId(userId);
+
+        // Assert
+        Assert.Null(returnedId);
+    }
+
+    [Fact]
     public void AddInfo_WorksCorrectly_WithValidInput()
     {
         // Arrange
@@ -108,20 +140,52 @@ public class PersonalInfoServiceTests
         _mapperMock.Setup(m => m.MapPersonalInfoUpload(It.IsAny<PersonalInfoUploadRequest>())).Returns(testPersonalInfo);
         
         // Act
-        _sut.AddInfo(uploadRequest, userId);
+        var entrySubmisson = _sut.AddInfo(uploadRequest, userId);
         
         // Assert
-        Assert.Equal(uploadRequest.Email, testPersonalInfo!.Email);
+        Assert.True(entrySubmisson.Success);
     }
 
     // AddInfo(PersonalInfoUploadRequest uploadRequest, Guid userId, out PersonalInfo? createdEntry);
         // with invalid info
     
+    [Fact]
+    public void AddInfo_ReturnsFailure_WhenInfoIsInvalid()
+    {
+        // Arrange
+        Guid userId = _fixture.Create<Guid>();
+        var uploadRequest = _fixture.Create<PersonalInfoUploadRequest>();
+        uploadRequest.Email = "test@asdasd.asd";
 
+            // set up repo returns
+        _repoMock.Setup(r => r.AddInfo(It.IsAny<PersonalInfo>(), userId)).Returns(true);
+        _repoMock.Setup(r => r.UserHasExistingPersonalInfo(userId)).Returns(false);
+        
+            // set up image converter returns
+        var _imageConverterMock = new Mock<IImageConverter>();
+        var imageUploadRequestMock = _fixture.Create<ImageUploadRequest>();
+        _imageConverterMock.Setup(i => i.ConvertImage(imageUploadRequestMock)).Returns(It.IsAny<byte[]>());
+
+        var testPersonalInfo = _fixture.Create<PersonalInfo>();
+        testPersonalInfo.Email = "test@asdasd.asd";
+        _mapperMock.Setup(m => m.MapPersonalInfoUpload(It.IsAny<PersonalInfoUploadRequest>())).Returns(testPersonalInfo);
+        
+        // Act
+        var entrySubmisson = _sut.AddInfo(uploadRequest, userId);
+        
+        // Assert
+        Assert.False(entrySubmisson.Success);
+        Assert.Equal("Failed to add entry to the database.", entrySubmisson.Message);      
+    }
 
     // AddInfo(PersonalInfoUploadRequest uploadRequest, Guid userId, out PersonalInfo? createdEntry);
         // with existing info
 
+    [Fact]
+    public void AddInfo_ReturnsFailure_UserHasExistingInfo()
+    {
+        
+    }
 
 
     // public bool UpdateInfo<T>(Guid userId, string propertyName, T newValue);
